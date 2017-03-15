79570ba2e8aec6d263705f731a6ada7bcb68d2a5
Liz Walcher <artisticaviary@gmail.com>
2017-03-15 00:41:00 -0700

    Initial commit

---
+++ CHOP_CodingTests/Assets/SceneToggle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneToggle : MonoBehaviour
+{
+    public GameObject panel;
+
+    public void Update()
+    {
+        if (Input.GetKeyUp("t"))
+        {
+			Application.LoadLevel("PancakeScene01");
+            
+            }
+		if (Input.GetKeyUp("g"))
+		{
+			Application.LoadLevel("SoupScene01");
+
+        }
+			
+		if (Input.GetKeyUp ("h")) {
+
+			Application.LoadLevel("MovementTests2");
+		}
+    }
+}
