ee61ff5b9a8d300e75bde5c8a5718df2a77247ad
William Walcher <wjwalcher@gmail.com>
2017-03-17 09:32:28 -0500

    Adds WASD Movement

--- CHOP_CodingTests/Assets/Scripts/ClickToMove.cs
+++ CHOP_CodingTests/Assets/Scripts/ClickToMove.cs
@@ -9,14 +9,18 @@ public class ClickToMove : MonoBehaviour {
     public Camera Cam;
     //public GameObject waypoint;
     //private GameObject wpInstance;
-
+	private Vector3 distToWalk = new Vector3(.8f,0,1.5f);
+	private float currentPos;
+	private Vector3 destVector;
+    private bool keepWalking;
 
 	void Awake ()
     {
         animator = GetComponent<Animator>();
         navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        destVector = navMeshAgent.transform.position;
 	}
-	
+
 
 	void Update ()
     {
@@ -38,6 +42,36 @@ public class ClickToMove : MonoBehaviour {
                     //Destroy(wpInstance, 0.5f);
                 }
 
+            }
+			if (Input.GetKey(KeyCode.W)) {
+                walking = true;
+				destVector.z = navMeshAgent.transform.position.z + distToWalk.z;
+				navMeshAgent.destination = destVector; 
+				navMeshAgent.Resume();
+			}
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                walking = true;
+                destVector.z = navMeshAgent.transform.position.z - distToWalk.z;
+                navMeshAgent.destination = destVector;
+                navMeshAgent.Resume();
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                walking = true;
+                destVector.x = (navMeshAgent.transform.position.x - distToWalk.x);
+                navMeshAgent.destination = destVector;
+                navMeshAgent.Resume();
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                walking = true;
+                destVector.x = navMeshAgent.transform.position.x + distToWalk.x;
+                navMeshAgent.destination = destVector;
+                navMeshAgent.Resume();
             }
 
             if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
