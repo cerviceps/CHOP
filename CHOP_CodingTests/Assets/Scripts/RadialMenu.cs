c15fda06c1bc0c0f9719dec36b544884c902db41
Will Walcher <wjwalcher@gmail.com>
2017-03-18 00:44:11 -0500

    Starting Radial Menu

---
+++ CHOP_CodingTests/Assets/Scripts/RadialMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialMenu : MonoBehaviour {
+
+    public RadialButton buttonPrefab;
+    public RadialMenu selected;
+
+	// Use this for initialization
+	public void SpawnButtons (Interactable obj) {
+        for (int i = 0; i < obj.options.Length; i++)
+        {
+            RadialButton newButton = Instantiate(buttonPrefab) as RadialButton;
+            newButton.transform.SetParent(transform, false);
+            float theta = (2 * Mathf.PI / obj.options.Length) * i;
+            float xPos = Mathf.Sin(theta);
+            float yPos = Mathf.Cos(theta);
+            newButton.transform.localPosition = new Vector3(xPos, yPos, 0f) * 80f;
+            newButton.circle.color = obj.options[i].color;
+            newButton.icon.sprite = obj.options[i].sprite;
+            newButton.title = obj.options[i].title;
+        }
+    }
+	
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
