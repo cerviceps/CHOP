4293860994e90cc42b7109c2a374e5b3da4bda4a
Taylor <taylor.cunningham@ackila.com>
2017-04-13 01:16:15 -0700

    Added scripts for Cooking System

---
+++ CHOP_CodingTests/Assets/Scripts/CookingSystem/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour {
+
+	[Header("Name needs to exactly match the ingredient name in CookingManager")]
+	public string name = null;
+
+	void Start() {
+		if (this.name == null) {
+			this.name = "DefaultIngredientName";
+		}
+	}
+
+}
