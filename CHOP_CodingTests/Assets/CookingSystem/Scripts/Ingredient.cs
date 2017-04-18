11dcc5faff9689887d661e39ba67336519346b99
Taylor <taylor.cunningham@ackila.com>
2017-04-17 20:45:49 -0700

    Added some prefabs, tweaked the IngredientSpawner, moved CookingSystem to its own folder, added Draggable script

---
+++ CHOP_CodingTests/Assets/CookingSystem/Scripts/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour {
+
+	[Header("Name needs to exactly match")]
+	[Header("the ingredient name in CookingManager")]
+	public string name = "DefaultIngredientName";
+
+	void Start() {
+		// Attach a Draggable monobehavior to this gameobject
+		gameObject.AddComponent<Draggable>();
+	}
+
+}
---
+++ CHOP_CodingTests/Assets/CookingSystem/Scripts/IngredientSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSpawner : MonoBehaviour {
+
+	// Takes an array of ingredients and spawns them at predetermined coords
+
+	[Header("Coords should correspond with prefabs")]
+	public Vector3[] coords;
+	public GameObject[] prefabs;
+	Dictionary<string, int> ingNameToIndexMap = null;
+
+	void Start() {
+		spawnAtAllCoords ();
+	}
+
+	public void spawnAtAllCoords() {
+		int length = Mathf.Min (this.prefabs.Length, this.coords.Length);
+		for (int i = 0; i < length; i++) {
+			Instantiate (prefabs [i], coords [i], Quaternion.identity);
+		}
+	}
+
+	public bool spawnByName(string name) {
+		if (this.ingNameToIndexMap == null) {
+			indexNames ();
+		}
+
+		int index;
+		try {
+			index = ingNameToIndexMap [name];
+		} catch (KeyNotFoundException e) {
+			Debug.LogError ("spawnByName() error: " + e);
+			return false;
+		}
+		// Instantiate the pref at the corresponding position (vector)
+		GameObject prefab = this.prefabs [index];
+		Vector3 coord = this.coords [index];
+		Instantiate (prefab, coord, Quaternion.identity);
+		return true;
+	}
+
+	// Index the Ingredient names the first time we look them up so we don't have to search through all the prefabs when we spawn them
+	private void indexNames() {
+		this.ingNameToIndexMap = new Dictionary<string, int> ();
+		for (int i = 0; i < this.prefabs.Length; i++) {
+			Ingredient prefabIngredient = (Ingredient)this.prefabs [i].GetComponent (typeof(Ingredient)) as Ingredient;
+			this.ingNameToIndexMap.Add (prefabIngredient.name, i);
+		}
+	}
+}
