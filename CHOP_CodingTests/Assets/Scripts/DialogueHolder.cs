f2e5860b250fd210624bda651c98e76e2eacf0b2
Liz Walcher <artisticaviary@gmail.com>
2017-04-25 04:25:52 -0700

    Adding Things
    Added:
    -new lumberjack model
    -some new particle effects (falling leaves, prefab for woodchips)
    -click-to-walk marker functionality
    -messing around with super text mesh  & dialogue
    -misc. other things, like animations on car lights, new walk cycles,
    etc. etc.

---
+++ CHOP_CodingTests/Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+	public string dialogue;
+
+	private DialogueManager RiverDialogue01;
+
+	// Use this for initialization
+	void Start () {
+
+		RiverDialogue01 = FindObjectOfType<DialogueManager> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider Other)
+	{
+		if (Other.gameObject.name == "Player") {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				RiverDialogue01.ShowBox (dialogue);
+			}
+		}
+	}
+
+}
---
+++ CHOP_CodingTests/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+	//public Animation FadeOut;
+
+	public GameObject dBox;
+	public SuperTextMesh dText;
+
+
+
+	public bool dialogueActive;
+
+	// Use this for initialization
+	void Start () {
+		//Animation = GetComponent<Animation> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (dialogueActive && Input.GetKeyDown(KeyCode.Space))
+		{
+			//Animation.
+			//Animation.play ("DialogueManager_FadeOut");
+
+			dBox.SetActive (false);
+			dialogueActive = false;
+		}
+
+	}
+
+	public void ShowBox(string dialogue) 
+	{
+		dialogueActive = true;
+		dBox.SetActive (true);
+		dText.text = dialogue;
+	}
+}
